ce4e18f83afbe82367b7bcedf6c069c43ffcac3c
Jonathan M. Guberman <jonathan@upnotnorth.net>
2013-12-06 11:22:19 -0500

    Fix for overflow on classic pro controllers

--- main.cpp
+++ main.cpp
@@ -229,7 +229,7 @@ int main(void){
             } else if (ext_id[2] == 0xA4 && ext_id[3] == 0x20 && ext_id[4] == 0x01 && ext_id[5] == 0x01)
             { //Classic or Pro
               env_phase = 322;
-              uint8_t rtrig_temp = 32-extension_classic_rtrig_analogue();
+              uint8_t rtrig_temp = 31-extension_classic_rtrig_analogue();
               env_subtract = ((rtrig_temp*rtrig_temp*rtrig_temp) >> 10) + 1;
               uint8_t classic_byax = extension_classic_byax();
               uint8_t classic_dpad = extension_classic_dpad();
